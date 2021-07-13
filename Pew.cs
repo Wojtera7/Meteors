0708c0cb8d52c25aa49ad9523d5e727e0104bc08
mwvstudio@outlook.com <mwvstudio@outlook.com>
2021-07-13 18:14:26 +0200

    Add project files.

---
+++ Pew.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meteors
+{
+    class Pew
+    {
+
+        public int Xpos { get; set; }
+        public int Ypos { get; set; }
+        public int ID { get; set; }
+
+        public Random rand = new Random();
+
+
+        public Pew(int id)
+        {
+            ID = id;
+            Xpos = rand.Next(9);
+            Ypos = 0;
+
+
+        }
+
+
+        public void Update()
+        {
+
+            Ypos++;
+            if (Ypos == 10)
+            {
+                Ypos = 0;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meteors
+{
+    class Program
+    {
+        static bool Lost = false;
+        static int turns = 0;
+        static List<Pew> pew = new List<Pew>();
+        static string[,] field = new string[10, 10];
+
+
+        static void Main(string[] args)
+        {
+
+            while (!Lost)
+            {
+
+                pew.Add(new Pew(turns));
+
+                for (int k = 0; k < pew.Count; k++)
+                {
+
+                    pew[k].Update();
+
+                }
+
+                SetField();
+
+                DrawField();
+
+                Console.ReadLine();
+
+
+
+                turns++;
+
+                if (turns >= 20)
+                {
+                    Lost = true;
+                }
+
+            }
+
+
+
+
+
+
+
+        }
+
+        public static void SetField()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+
+                    field[i, j] = " ";
+
+
+                }
+
+
+            }
+
+
+            for (int l = 0; l < turns; l++)
+            {
+
+                field[pew[l].Xpos, pew[l].Ypos] = "#";
+
+
+            }
+
+
+        }
+
+        public static void DrawField()
+        {
+
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+
+                    Console.Write(field[j, i]);
+
+                }
+
+                Console.WriteLine("");
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
